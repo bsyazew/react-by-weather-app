dea0e9fa4617dda98ab788fefbc669bae2e8a3ca
bsyazew <bsyazew@gmail.com>
2023-08-19 19:05:02 +0100

    added rel element to link in app.js
     rel="noreferrer" to help deploy on netlify

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
       <div className="weather-app-wrapper">
         <div className="card">
           <div className="card-body">
-          
-
             <Weather defaultCity="New York" />
 
             <div className="weather-forecast" id="forecast"></div>
@@ -17,6 +15,7 @@ function App() {
         </div>
       </div>
       <a
+        rel="noreferrer"
         href="https://github.com/bsyazew/react-by-weather-app"
         target="_blank"
         className="link"
