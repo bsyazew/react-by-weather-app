9eaca411a6c67939506a96d868597a4b18648f34
bsyazew <bsyazew@gmail.com>
2023-08-21 11:33:20 +0100

    made technical adjustments to help w deploying

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import WeatherIcon from "./WeatherIcon";
+
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./App.css";
 import axios from "axios";
@@ -8,10 +8,9 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
-useEffect(() => {
-  setLoaded(false);
-}, [props.coords]);
-
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coords]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -22,17 +21,17 @@ useEffect(() => {
     return (
       <div className="weather-forecast">
         <div className="row">
-          {forecast.map(function(dailyForecast, index){
+          {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
-return (
-  <div className="col" key={index}>
-    <WeatherForecastDay data={dailyForecast} />
-  </div>
-);} else {
-  return null;
-}
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
           })}
-        
         </div>
       </div>
     );
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,19 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 export default function WeatherTemperature(props) {
- 
-    return (
-      <div>
-        <span className="degrees" id="todays-temperature">
-          {Math.round(props.celsius)}
-        </span>
-        <span className="deg-celsius">
-          ℃ 
-       
-        </span>
-      </div>
-  
-    );
-  }
-
+  return (
+    <div>
+      <span className="degrees" id="todays-temperature">
+        {Math.round(props.celsius)}
+      </span>
+      <span className="deg-celsius">℃</span>
+    </div>
+  );
+}
